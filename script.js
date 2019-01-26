88fa296244d8bc906b160f92148edc7431948d2f
notbrad <45443057+notbrad@users.noreply.github.com>
2019-01-26 12:50:27 -0800

    Added menu animations, links

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-function dropDown(linkChoice) {
-  
+
+function dropDown() {
+  var navbar = document.getElementById('navbar');
+  var arrow = document.getElementById('arrow');
+  var dropbar = document.getElementById('dropbar');
+
+  if (arrow.classList.contains('fa-angle-down')) {
+    dropbar.classList.add('fade-in')
+    navbar.classList.remove('dropup')
+    navbar.classList.add('drop-down')
+    navbar.classList.add('animate-drop-down')
+    arrow.classList.remove('fa-angle-down')
+    arrow.classList.add('fa-angle-up')
+    dropbar.classList.remove('hidden')
+    dropbar.classList.remove('fade-out')
+  } 
+
+ else {
+    dropbar.classList.remove('fade-in')
+    dropbar.classList.add('fade-out')
+    navbar.classList.remove('animate-drop-down')
+    navbar.classList.remove('drop-down')
+    navbar.classList.add('dropup')
+    arrow.classList.toggle('fa-angle-down')
+    arrow.classList.toggle('fa-angle-up')
+    dropbar.classList.toggle('hidden')
+ }
+
 }
